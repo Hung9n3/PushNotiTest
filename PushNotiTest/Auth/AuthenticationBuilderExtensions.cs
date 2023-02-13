ce3fce7db63df52b130580eb88de7b92637d2b39
Hung9n3 <chovameo352@gmail.com>
2023-02-13 13:13:55 +0700

    init

---
+++ PushNotiTest/Auth/AuthenticationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using PushDemoAPI.Authentication;
+
+namespace PushNotiTest.Auth
+{
+    public static class AuthenticationBuilderExtensions
+    {
+        public static AuthenticationBuilder AddApiKeyAuth(
+            this AuthenticationBuilder builder,
+            Action<ApiKeyAuthOptions> configureOptions)
+        {
+            return builder
+                .AddScheme<ApiKeyAuthOptions, ApiKeyAuthHandler>(
+                    ApiKeyAuthOptions.DefaultScheme,
+                    configureOptions);
+        }
+    }
+}
